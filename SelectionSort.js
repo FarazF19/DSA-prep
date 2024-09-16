488752abe3403593f6cf04c3b4eaf9c29e9fac66
FarazF19 <mfaraz.bees19@iba-suk.edu.pk>
2024-09-16 23:24:26 +0500

    added LinkedList

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,22 +1,22 @@
 // Array Sorting with SelectionSort - Ascending Order
-function selectionSort(data){
+function selectionSort(data) {
     let minVal;
     for (let i = 0; i < data.length; i++) {
         minVal = i;
 
-        for (let j = i+1; j< data.length; j++) {          
-            if (data[j]<data[minVal]){        // for descending order change < to > in condition
-                minVal=j;
+        for (let j = i + 1; j < data.length; j++) {
+            if (data[j] < data[minVal]) {        // for descending order change < to > in condition
+                minVal = j;
             }
-           
         }
-    // Swapping minValue    
-    let temp = data[minVal];
-    data[minVal] = data[i];
-    data[i] = temp; 
+
+        // Swapping minValue    
+        let temp = data[minVal];
+        data[minVal] = data[i];
+        data[i] = temp;
     }
-   
+
 }
-let arr = [20,12,53,3];
+let arr = [20, 12, 53, 3];
 selectionSort(arr);
 console.log(arr);
