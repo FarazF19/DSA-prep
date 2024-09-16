281890a54cf0ef5b9b0422f567ccbefacdd7312c
FarazF19 <mfaraz.bees19@iba-suk.edu.pk>
2024-09-17 00:10:53 +0500

    added LL node append,traversal and deletion

--- LinkedList.js
+++ LinkedList.js
@@ -18,12 +18,48 @@ class List {
         this.tail = newNode;
         this.size += 1;
     }
+
+    traverseList() {
+        let counter = 0;
+        let currentNode = this.head;
+
+        while (counter < this.size) {
+            console.log(currentNode);
+            currentNode = currentNode.next;
+            counter++;
+
+        }
+    }
+
+    deleteNode(index) {
+        let counter = 1;
+        let lead = this.head;
+
+        if (index === 1) {
+            this.head = this.head.next;
+        } else {
+            while (counter < index - 1) {
+                lead = lead.next;
+                counter++;
+            }
+            let nextNode = lead.next.next;
+            lead.next = nextNode;
+            this.size -= 1;
+        }
+
+    }
 }
 
 let list = new List(100);
 list.appendNode(200);
 list.appendNode(300);
 list.appendNode(400);
-list.appendNode(500);
-list.appendNode(600);
+
+//list.traverseList();
+
+list.deleteNode(2);
 console.log(list);
+
+
+
+
